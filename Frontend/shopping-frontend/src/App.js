599b477138885420c7cf1dd355ee6e83c00bd174
Shivam <shivamtandon673@gmail.com>
2021-07-31 22:41:37 +0530

    shopping Frontend commit - Product screen

--- Frontend/shopping-frontend/src/App.js
+++ Frontend/shopping-frontend/src/App.js
@@ -1,8 +1,12 @@
-import data from './data';
+import React from 'react';
+import { BrowserRouter,Route } from 'react-router-dom';
+import ProductScreen from './Screen/ProductScreen';
+import HomeScreen from './Screen/HomeScreen';
 
 function App() {
 
     return (
+        <BrowserRouter>
         <div className="grid-container">
             <header className="row">
                 <div>
@@ -14,39 +18,14 @@ function App() {
                 </div>
             </header>
             <main>
-                <div className="row center">
-                    {
-                        data.products.map((product) => (
-                            <div key={product._id} className="card">
-                                <a href={`/product/${product._id}`}>
-                                    <img className="medium" src={product.image} alt={product.name} />
-                                </a>
-                                <div className="card-body">
-                                    <a href={`/product/${product._id}`}>
-                                        <h2>{product.name}</h2>
-                                    </a>
-                                    <div className="rating">
-                                        <span> <i className="fa fa-star"></i> </span>
-                                        <span> <i className="fa fa-star"></i> </span>
-                                        <span> <i className="fa fa-star"></i> </span>
-                                        <span> <i className="fa fa-star"></i> </span>
-                                        <span> <i className="fa fa-star"></i> </span>
-                                    </div>
-                                    <div className="price">
-                                        ${product.price}
-                                    </div>
-                                </div>
-                            </div>
-                        )
-
-                        )
-                    }
-                </div>
+                <Route path = "/product/:id" component = {ProductScreen}></Route>
+                <Route path = "/" component = {HomeScreen} exact></Route>
             </main>
             <footer className="row center">
                 All Rights Reserved
             </footer>
         </div>
+        </BrowserRouter>
     );
 }
 
