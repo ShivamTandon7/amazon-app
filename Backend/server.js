0d2c8749847468680390bb5010fef38c31bae158
Shivam <shivamtandon673@gmail.com>
2021-12-26 01:08:52 +0530

    publish to heroku

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import path from 'path';
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -19,18 +19,20 @@ app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res)=>{
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb'); 
+app.get('/api/config/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-})
+app.use(express.static(path.join(__dirname, '/Frontend/shopping-frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/Frontend/shopping-frontend/build/index.html')));
+// app.get('/', (req, res) => {
+//     res.send('Server is ready');
+// })
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message});
+    res.status(500).send({ message: err.message });
 })
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
